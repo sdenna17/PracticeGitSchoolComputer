5760c6626494850d7974e7a99c9167f7c2c3fcee
Shannon Denna <sdenna@c07vw1png1hw.local>
2019-04-25 08:42:49 -0500

    Initial Commit

---
+++ PracticeGitSchoolComputer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeGitSchoolComputer
+//
+//  Created by Shannon M. Denna on 4/25/19.
+//  Copyright © 2019 Shannon M. Denna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
